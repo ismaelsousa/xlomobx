4a575776e83457b0c54989527ecf927d1414d4f2
ismaelsousa <ismaelsousa1998@outlook.com>
2021-11-10 14:15:48 -0300

    create login screen complete

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:xlomobx/screens/signup/signup_screens.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -14,79 +15,120 @@ class _LoginScreenState extends State<LoginScreen> {
       appBar: AppBar(
         title: const Text('Entrar'),
       ),
-      body: Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-        margin: const EdgeInsets.symmetric(horizontal: 32),
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const Text(
-                "Acessar com E-mail",
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.grey,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 3, bottom: 4, top: 8),
-                child: Text(
-                  'E-mail',
-                  style: TextStyle(
-                      color: Colors.grey[800],
+      body: Container(
+        alignment: Alignment.center,
+        child: SingleChildScrollView(
+          child: Card(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+            margin: const EdgeInsets.symmetric(horizontal: 32),
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text(
+                    "Acessar com E-mail",
+                    style: TextStyle(
                       fontSize: 16,
-                      fontWeight: FontWeight.w700),
-                ),
+                      color: Colors.grey,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 3, bottom: 4, top: 8),
+                    child: Text(
+                      'E-mail',
+                      style: TextStyle(
+                          color: Colors.grey[800],
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700),
+                    ),
+                  ),
+                  const TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      isDense: true,
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Padding(
+                      padding:
+                          const EdgeInsets.only(left: 3, bottom: 4, top: 8),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Senha',
+                            style: TextStyle(
+                                color: Colors.grey[800],
+                                fontSize: 16,
+                                fontWeight: FontWeight.w700),
+                          ),
+                          GestureDetector(
+                              onTap: () {},
+                              child: const Text('Esqueci minha senha',
+                                  style: TextStyle(
+                                      decoration: TextDecoration.underline,
+                                      color: Colors.purple)))
+                        ],
+                      )),
+                  const TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      isDense: true,
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  const Divider(),
+                  Container(
+                      height: 50,
+                      margin: const EdgeInsets.symmetric(vertical: 12),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('ENTRAR'),
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.orange,
+                            elevation: 0,
+                            textStyle: const TextStyle(color: Colors.white),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20))),
+                      )),
+                  const Divider(),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: Wrap(
+                      alignment: WrapAlignment.center,
+                      children: [
+                        const Text(
+                          "Não tenho conta! ",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (_) => const SignUpScreen()));
+                          },
+                          child: const Text(
+                            'Cadastre-se',
+                            style: TextStyle(
+                                decoration: TextDecoration.underline,
+                                color: Colors.purple,
+                                fontSize: 16),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
               ),
-              const TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  isDense: true,
-                ),
-                keyboardType: TextInputType.emailAddress,
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              Padding(
-                  padding: const EdgeInsets.only(left: 3, bottom: 4, top: 8),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Senha',
-                        style: TextStyle(
-                            color: Colors.grey[800],
-                            fontSize: 16,
-                            fontWeight: FontWeight.w700),
-                      ),
-                      GestureDetector(
-                          onTap: () {},
-                          child: const Text('Esqueci minha senha',
-                              style: TextStyle(
-                                  decoration: TextDecoration.underline,
-                                  color: Colors.purple)))
-                    ],
-                  )),
-              const TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  isDense: true,
-                ),
-                keyboardType: TextInputType.emailAddress,
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              Container(
-                  height: 40,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('Entrar'),
-                  ))
-            ],
+            ),
           ),
         ),
       ),
